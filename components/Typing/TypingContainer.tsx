f76630311bd4eca206c3f8b5f363bbe3d04d753a
ni3rav <mahetan35@gmail.com>
2025-02-06 11:00:19 +0530

    fix: adjust container height to use full available space

--- components/Typing/TypingContainer.tsx
+++ components/Typing/TypingContainer.tsx
@@ -6,7 +6,7 @@ import TopBar from "./TopBar";
 export function TypingContainer() {
   return (
     <MetricsProvider>
-      <div className="w-full h-[85vh] flex flex-col justify-center items-center gap-8">
+      <div className="w-full h-full flex flex-col justify-center items-center gap-8">
         <TopBar />
         <TypingText />
       </div>
